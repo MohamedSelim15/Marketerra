985d4881848e3824ad518c3d0bd9208a42c0a378
MohamedSelim15 <hamadselim61@gmail.com>
2025-08-16 22:37:41 +0300

    Refactor Header component for improved mobile navigation and clean up unused imports; update OurWork component for consistent image styling and shadow effects

--- src/Porfolio Components/OurWork.jsx
+++ src/Porfolio Components/OurWork.jsx
@@ -10,14 +10,16 @@ const OurWork = () => {
         "/Folk Burger/Screenshot 2025-08-16 195253.png",
         "/Folk Burger/Screenshot 2025-08-16 195238.png",
         "/Folk Burger/Screenshot 2025-08-16 195315.png",
-
       ],
       image2: [
         "/Folk Burger/Screenshot 2025-08-16 195325.png",
         "/Folk Burger/Screenshot 2025-08-16 195331.png",
         "/Folk Burger/Screenshot 2025-08-16 195343.png",
       ],
-      imageShadow: ["0 -5px 10px rgba(242, 166, 59, 0.5)","0 5px 10px rgba(242, 166, 59, 0.5)"]
+      imageShadow: [
+        "0 -5px 10px rgba(242, 166, 59, 0.5)",
+        "0 5px 10px rgba(242, 166, 59, 0.5)",
+      ],
     },
     {
       title: "Andalusia Cafe",
@@ -27,19 +29,18 @@ const OurWork = () => {
         "../../public/Andalusia Cafe/Screenshot 2025-08-16 213605.png",
         "../../public/Andalusia Cafe/Screenshot 2025-08-16 213622.png",
         "../../public/Andalusia Cafe/Screenshot 2025-08-16 213646.png",
-        
-        
       ],
       image2: [
         "../../public/Andalusia Cafe/Screenshot 2025-08-16 213656.png",
         "../../public/Andalusia Cafe/Screenshot 2025-08-16 213704.png",
         "../../public/Andalusia Cafe/Screenshot 2025-08-16 213710.png",
         "../../public/Andalusia Cafe/Screenshot 2025-08-16 213723.png",
-        
       ],
-      imageShadow: ["0 -5px 10px rgba(73, 165, 185, 0.5)","0 5px 10px rgba(73, 165, 185, 0.5)"]
+      imageShadow: [
+        "0 -5px 10px rgba(73, 165, 185, 0.5)",
+        "0 5px 10px rgba(73, 165, 185, 0.5)",
+      ],
     },
-  
   ];
 
   return (
@@ -51,7 +52,10 @@ const OurWork = () => {
       </div>
 
       {clients.map((client, index) => (
-        <div key={index} className="flex flex-col justify-start items-center w-screen mb-10">
+        <div
+          key={index}
+          className="flex flex-col justify-start items-center w-screen mb-10"
+        >
           <div style={{ color: client.titleColor }} className="font-bold">
             <h1 className="text-5xl text-center">{client.title}</h1>
             <p className="text-2xl text-center mt-4 font-light">
@@ -63,24 +67,23 @@ const OurWork = () => {
             {client.image.map((img, imgIndex) => (
               <img
                 key={imgIndex}
-                width={450}
-                className="rounded-lg shadow-2xs"
-                style={{ boxShadow: client.imageShadow[0], clientColor: client.titleColor }}
+                className="rounded-lg shadow-2xs xl:w-[450px] lg:w-[450px] w-[350px]"
+                style={{
+                  boxShadow: client.imageShadow[0],
+                  clientColor: client.titleColor,
+                }}
                 src={img}
                 alt=""
               />
-
             ))}
             {client.image2.map((img, imgIndex) => (
               <img
                 key={imgIndex}
-                width={300}
-                className="rounded-lg shadow-xl"
+                className="rounded-lg shadow-xl xl:w-[300px] lg:w-[300px] w-[350px]"
                 style={{ boxShadow: client.imageShadow }}
                 src={img}
                 alt=""
               />
-
             ))}
           </div>
         </div>
